071e68dce59678ecb614a7e0402b0d96da74c5c6
Gonzalo Aravena <gonzaloam@icloud.com>
2025-09-08 11:14:13 -0300

    logica-js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+//funcion de agregar amigos con su respectiva validacion 
+function agregarAmigo() {
+    let nombreDeAmigo = document.getElementById("amigo").value;
+
+    // validar que no esté vacío
+    if (nombreDeAmigo.trim() === "") {
+        alert("Digite un nombre de amigo!");
+        return;
+    }
+    //validar que no se repita
+    if (listaDeAmigos.includes(nombreDeAmigo)) {
+        alert("Este amigo ya está en la lista!");
+        return;
+    }
+    // guardar en el array
+    listaDeAmigos.push(nombreDeAmigo);
+
+    // limpiar el input
+    document.getElementById("amigo").value = "";
+
+    // actualizar la lista en pantalla
+    mostrarAmigos();
+}
+// función para mostrar la lista en el <ul>
+function mostrarAmigos() {
+    let ul = document.getElementById("listaAmigos");
+
+    // limpiar lista antes de volver a pintarla
+    ul.innerHTML = "";
+
+    // recorrer el array y crear <li> para cada amigo
+    listaDeAmigos.forEach(function (amigo) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        ul.appendChild(li);
+    });
+}
+
+
